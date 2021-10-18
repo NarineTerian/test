827ff832f68a959ebf2f5c3a69c62efc24fc76ae
NarineTerian <92754619+NarineTerian@users.noreply.github.com>
2021-10-19 00:36:50 +0300

    homework

---
+++ alert.js
@@ -0,0 +1,38 @@
+
+//1.сделать функцию, которая принимает массив чисел, возвращает массив, где каждый элемент умножен на 10
+//2.сделать функцию, которая принимает массив числел и возвращает массив числе,
+// но при этом фильтрует их по четности. Возвращает только четные числа
+/*
+const mapFn = (arr) => {
+  let result = [];
+
+  for (let i = 0; i <= arr.length - 1; i++;) {
+    const current = arr(i);
+    resultArr.push(current * 10);
+  }
+  return resultArr
+};
+console.log(mapFn());
+
+
+
+const arr = [1, 2, 3, 4]
+let result = arr.filter(elem => elem % 2 == 0);
+  console.log(result);
+
+  /*
+  let result = arr.filter(function(elem) {
+  	if (elem % 2 == 0) {
+  		return true;
+  	} else {
+  		return false;
+  	}
+  });
+  */
+
+  /* for (let i = 0; i < arr.length; i++;) {
+      if((arr[i] % 2) === 0){
+          arr[i];
+        }
+  };
+*/
